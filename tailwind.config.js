751cbdf44fb6ef232189b2fd2c65bdeb8c888ddb
Xexer Ibrahimli <xezerib2008@gmail.com>
2025-10-04 09:35:11 +0400

    Add New Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        mycolor: `#1B1B1B`,
+        mycolors:{
+          100: `#1A1A1A`,
+          200: `#1C1C1C`
+        }
+      },
+      fontFamily:{
+        myfont:["Jersey"]
+      }
+    },
   },
   plugins: [],
 }
